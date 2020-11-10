b40ccf07177c96fec17b1546c9df09177a58e2f2
Mike Abrahall <mike@abrahall.id.au>
2020-11-10 22:04:25 +1100

    Turn the placer code into actual placer code

--- temp-placer.lua
+++ temp-placer.lua
@@ -4,14 +4,42 @@ num_of_rows = 34
 -- FIXME: Assumes it has enough fuel
 -- FIXME: Doesn't care if inv is full
 
-turtle.dig()
+
+io.write("Need ")
+io.write(num_of_lines*num_of_rows)
+io.write(" items to place. That's ")
+io.write((num_of_lines*num_of_rows)/64)
+print("stacks")
+print("I'm just assuming I have enough and running off now")
+
+function get_next_item()
+    if turtle.getItemCount() >= 1 then
+        return turtle.getSelectedSlot()
+    else
+        for i = 1, 16, 1 do
+            turtle.select(i)
+            if turtle.getItemCount() >= 1 then
+                return i
+            end
+        end
+    end
+    error("Ran out of items")
+end
+
+function place_next_item()
+    get_next_item()
+    turtle.place()
+end
+
 turtle.forward()
+turtle.turnRight()
+turtle.turnRight()
 for line=1,num_of_lines,1 do
     io.write("Starting line #")
     print(line)
     for row=2,num_of_rows,1 do
-        turtle.dig()
-        turtle.forward()
+        turtle.back()
+        place_next_item()
     end
     -- Turn right on even lines, left on odd ones
     if (line ~= num_of_lines) then
@@ -20,8 +48,8 @@ for line=1,num_of_lines,1 do
         else
             turtle.turnLeft()
         end
-        turtle.dig()
-        turtle.forward()
+        turtle.back()
+        place_next_item()
         if (line % 2 == 0) then
             turtle.turnRight()
         else
