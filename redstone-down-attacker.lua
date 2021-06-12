2c36c0d4adce332172082aa4f5a094a4581e0a7a
Mike Abrahall <mike@abrahall.id.au>
2021-06-13 00:12:49 +1000

    Use os.pullEvent instead of a wait-loop

---
+++ redstone-down-attacker.lua
@@ -0,0 +1,31 @@
+function attack_for60s()
+    print("Running attack loop for 1 minute to kill the leftover mobs")
+    start_time = os.clock()
+    repeat
+        turtle.attackDown()
+        sleep(0)  -- To ensure any parallel functions get a bit of a turn
+    until os.clock() >= (start_time + 60)
+    print("Finished 1 minute loop")
+end
+
+function main()
+    if redstone.getInput("back") then
+        print("Mob farm is off, waiting for it to turn on")
+        -- We'll just restart the loop as soon as there's *any* redstone update
+        -- This if statement will run again and we'll see what happens.
+        os.pullEvent("redstone")
+    else
+        print("Starting attack loop")
+        repeat
+            turtle.attackDown()
+            sleep(0)  -- To ensure any parallel functions get a bit of a turn
+        until redstone.getInput("back")
+
+        print("Redstone switch activated")
+        attack_for60s()
+    end
+end
+
+while true do
+    main()
+end
